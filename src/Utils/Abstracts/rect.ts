7126fc7e1267da3832b47e7f6005b19d70af3e3f
Luís Santos <thewaver@gmail.com>
2026-05-16 05:35:58 +0300

    stuff

--- src/Utils/Abstracts/rect.ts
+++ src/Utils/Abstracts/rect.ts
@@ -11,7 +11,14 @@ export type Rect = Vec4d<typeof K1, typeof K2, typeof K3, typeof K4>;
 export type RectString = Vec4dString<typeof K1, typeof K2, typeof K3, typeof K4>;
 
 export namespace Rect {
+    export const min = Vec4d.min(K1, K2, K3, K4);
+    export const max = Vec4d.max(K1, K2, K3, K4);
+    export const add = Vec4d.add(K1, K2, K3, K4);
+    export const sub = Vec4d.sub(K1, K2, K3, K4);
+    export const mul = Vec4d.mul(K1, K2, K3, K4);
+    export const div = Vec4d.div(K1, K2, K3, K4);
     export const isSame = Vec4d.isSame(K1, K2, K3, K4);
+    export const spread = Vec4d.spread(K1, K2, K3, K4);
     export const toString = Vec4d.toString(K1, K2, K3, K4);
 }
 
--- src/Utils/Abstracts/vec4d.ts
+++ src/Utils/Abstracts/vec4d.ts
@@ -10,10 +10,84 @@ export type Vec4dString<
 > = `${Uppercase<A>}${number}_${Uppercase<B>}${number}_${Uppercase<C>}${number}_${Uppercase<D>}${number}`;
 
 export namespace Vec4d {
+    export const min =
+        <A extends string, B extends string, C extends string, D extends string>(k1: A, k2: B, k3: C, k4: D) =>
+        (a: Vec4d<A, B, C, D>, b: Vec4d<A, B, C, D>): Vec4d<A, B, C, D> | undefined =>
+            a !== undefined || b !== undefined
+                ? ({
+                      [k1]: Math.min(a[k1], b[k1]),
+                      [k2]: Math.min(a[k2], b[k2]),
+                      [k3]: Math.min(a[k3], b[k3]),
+                      [k4]: Math.min(a[k4], b[k4]),
+                  } as Vec4d<A, B, C, D>)
+                : undefined;
+
+    export const max =
+        <A extends string, B extends string, C extends string, D extends string>(k1: A, k2: B, k3: C, k4: D) =>
+        (a: Vec4d<A, B, C, D>, b: Vec4d<A, B, C, D>): Vec4d<A, B, C, D> | undefined =>
+            a !== undefined || b !== undefined
+                ? ({
+                      [k1]: Math.max(a[k1], b[k1]),
+                      [k2]: Math.max(a[k2], b[k2]),
+                      [k3]: Math.max(a[k3], b[k3]),
+                      [k4]: Math.max(a[k4], b[k4]),
+                  } as Vec4d<A, B, C, D>)
+                : undefined;
+
+    export const add =
+        <A extends string, B extends string, C extends string, D extends string>(k1: A, k2: B, k3: C, k4: D) =>
+        (a: Vec4d<A, B, C, D>, b: Vec4d<A, B, C, D>): Vec4d<A, B, C, D> =>
+            ({
+                [k1]: a[k1] + b[k1],
+                [k2]: a[k2] + b[k2],
+                [k3]: a[k3] + b[k3],
+                [k4]: a[k4] + b[k4],
+            }) as Vec4d<A, B, C, D>;
+
+    export const sub =
+        <A extends string, B extends string, C extends string, D extends string>(k1: A, k2: B, k3: C, k4: D) =>
+        (a: Vec4d<A, B, C, D>, b: Vec4d<A, B, C, D>): Vec4d<A, B, C, D> =>
+            ({
+                [k1]: a[k1] - b[k1],
+                [k2]: a[k2] - b[k2],
+                [k3]: a[k3] - b[k3],
+                [k4]: a[k4] - b[k4],
+            }) as Vec4d<A, B, C, D>;
+
+    export const mul =
+        <A extends string, B extends string, C extends string, D extends string>(k1: A, k2: B, k3: C, k4: D) =>
+        (a: Vec4d<A, B, C, D>, b: Vec4d<A, B, C, D>): Vec4d<A, B, C, D> =>
+            ({
+                [k1]: a[k1] * b[k1],
+                [k2]: a[k2] * b[k2],
+                [k3]: a[k3] * b[k3],
+                [k4]: a[k4] * b[k4],
+            }) as Vec4d<A, B, C, D>;
+
+    export const div =
+        <A extends string, B extends string, C extends string, D extends string>(k1: A, k2: B, k3: C, k4: D) =>
+        (a: Vec4d<A, B, C, D>, b: Vec4d<A, B, C, D>): Vec4d<A, B, C, D> =>
+            ({
+                [k1]: a[k1] / b[k1],
+                [k2]: a[k2] / b[k2],
+                [k3]: a[k3] / b[k3],
+                [k4]: a[k4] / b[k4],
+            }) as Vec4d<A, B, C, D>;
+
     export const isSame =
         <A extends string, B extends string, C extends string, D extends string>(k1: A, k2: B, k3: C, k4: D) =>
         (a?: Vec4d<A, B, C, D>, b?: Vec4d<A, B, C, D>): boolean =>
             !!a && !!b && a[k1] === b[k1] && a[k2] === b[k2] && a[k3] === b[k3] && a[k4] === b[k4];
+
+    export const spread =
+        <A extends string, B extends string, C extends string, D extends string>(k1: A, k2: B, k3: C, k4: D) =>
+        (value: number): Vec4d<A, B, C, D> =>
+            ({
+                [k1]: value,
+                [k2]: value,
+                [k3]: value,
+                [k4]: value,
+            }) as Vec4d<A, B, C, D>;
 
     export const toString =
         <A extends string, B extends string, C extends string, D extends string>(k1: A, k2: B, k3: C, k4: D) =>
